8870acfa9632be75fcbc50c9327cd442b39de498
Dannirl6378 <127243165+Dannirl6378@users.noreply.github.com>
2025-12-02 16:58:20 +0100

    feat: uprava detailu připrava na větev Home

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Component } from 'react';
+import { Typography } from '@mui/material';
+
+export default function Home(){
+    return (<>
+    <Typography variant="h4" gutterBottom>
+      Vítejte v aplikaci pro vyúčtování služeb
+    </Typography>
+   
+    </>)
+}
